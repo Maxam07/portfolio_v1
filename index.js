d9396c3821b9ec62597e3ab66891ce934b191b35
Max <maxuberas@gmail.com>
2025-02-19 17:16:01 +0800

    [CHORE] Slideshow/Carousel

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+let slideIndex = 1; // Ensure slideIndex is initialized
+
 udemy_cert = document.querySelector(".certificate_udemy");
 freeCodeCamp = document.querySelector(".certificate_freeCodeCamp");
 
+// View Certificate Button
 function viewCert(buttonNum, event) {
 
     switch(buttonNum) {
@@ -29,8 +32,36 @@ document.querySelector(".freeCodeCampBtn").addEventListener('click', function(ev
     viewCert(2, event)
 })
 
-
 document.addEventListener('click', function() {
     udemy_cert.style.display = "none"
     freeCodeCamp.style.display = "none"
 });
+
+// Slideshow Chevron
+    function plusSlides(n) {
+        slideIndex += n;
+        showSlides(slideIndex);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlide");
+
+        // Ensure slideIndex stays within bounds
+        if (n > slides.length) {
+            slideIndex = 1;
+        } else if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        // Hide all slides
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        // Show the current slide
+        slides[slideIndex - 1].style.display = "block";
+    }
+
+    // Initially show the first slide
+    showSlides(slideIndex);
